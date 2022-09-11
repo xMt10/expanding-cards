8f77d7ab6a73b366b530130090cfe706287b6328
xMt10 <mturpcu480@gmail.com>
2022-09-11 18:34:58 +0300

    project is done

---
+++ script.js
@@ -0,0 +1,19 @@
+ const panels = document.querySelectorAll(".panel");
+
+
+panels.forEach(panel => {
+  panel.addEventListener("click",() => {
+
+    removeActiveClass()
+    panel.classList.add("active")
+})
+})
+   
+
+
+const removeActiveClass = () => {
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
+
